b95623fe2d3bcff54c1fa376fc11da478f212302
Elvis Gannem <egannem@gmail.com>
2021-11-06 06:14:46 -0400

    Navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from  '../../images/logo.svg'
+import IconCart from '../../images/icon-cart.svg'
+import Avatar from '../../images/image-avatar.png'
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className={styles.firstContainer}>
+                <div class={styles.logo}>
+                    <img src={Logo} alt="Logo sneakers" />
+                </div>
+
+                <div>
+                    <ul>
+                        <li>Collections</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.rightContainer}>
+                <img src={IconCart} alt="Cart" />
+                <img src={Avatar} alt="Avatar" className={styles.avatar} />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
